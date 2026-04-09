c751ee2b7a2d9d753ade53a4a6fc349e7d74fd3b
Oliver Hartley <oliverhartley@google.com>
2026-04-09 12:58:46 -0400

    Add logHeaders function to fetch sheet headers

--- Code.js
+++ Code.js
@@ -1,3 +1,7 @@
-function myFunction() {
-  
+function logHeaders() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getActiveSheet();
+  var range = sheet.getRange(1, 1, 1, sheet.getLastColumn());
+  var headers = range.getValues()[0];
+  Logger.log("Headers: " + JSON.stringify(headers));
 }
