91d5c8eb43e64394d5232df4ef253127269524cd
Oliver Hartley <oliverhartley@google.com>
2026-04-09 13:00:40 -0400

    Add logFirst50Rows function to fetch data

--- Code.js
+++ Code.js
@@ -5,3 +5,11 @@ function logHeaders() {
   var headers = range.getValues()[0];
   Logger.log("Headers: " + JSON.stringify(headers));
 }
+
+function logFirst50Rows() {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getActiveSheet();
+  var range = sheet.getRange(1, 1, 50, sheet.getLastColumn());
+  var values = range.getValues();
+  Logger.log("DATA_START:" + JSON.stringify(values) + ":DATA_END");
+}
